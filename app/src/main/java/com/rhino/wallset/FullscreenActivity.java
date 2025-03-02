09ddc433422f8c93a23358ef23194ea6d1ad43ab
Mustafa Yönügül <95026273+mustafayngl@users.noreply.github.com>
2025-03-02 17:56:24 +0300

    setting as wallpaper added

--- app/src/main/java/com/rhino/wallset/FullscreenActivity.java
+++ app/src/main/java/com/rhino/wallset/FullscreenActivity.java
@@ -1,24 +1,36 @@
 package com.rhino.wallset;
 
+import android.app.WallpaperManager;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import com.bumptech.glide.Glide;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FullscreenActivity extends AppCompatActivity {
-    private Button addToFavoritesButton;
+    private Button addToFavoritesButton, btnSetWallpaper;
     private String wallpaperUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
+
+        // Toolbar'ı gizle
+        getSupportActionBar().hide();
 
         // Görseli görüntüle
         ImageView imageView = findViewById(R.id.fullscreenImageView);
@@ -35,10 +47,11 @@ public class FullscreenActivity extends AppCompatActivity {
 
         // Favoriye ekleme butonunu bul
         addToFavoritesButton = findViewById(R.id.addToFavoritesButton);
+        // Duvar kağıdı yapma butonunu bul
+        btnSetWallpaper = findViewById(R.id.btnSetWallpaper);
 
         // Favoriye ekleme işlemi
         addToFavoritesButton.setOnClickListener(v -> {
-            // Favorilerde olup olmadığını kontrol et
             Set<String> favoritesSet = sharedPreferences.getStringSet("favorites", new HashSet<>());
             if (favoritesSet.contains(wallpaperUrl)) {
                 Toast.makeText(FullscreenActivity.this, "Bu duvar kağıdı zaten favorilerde!", Toast.LENGTH_SHORT).show();
@@ -49,5 +62,31 @@ public class FullscreenActivity extends AppCompatActivity {
                 Toast.makeText(FullscreenActivity.this, "Duvar kağıdı favorilere eklendi!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        // Duvar kağıdı olarak ayarlama işlemi
+        btnSetWallpaper.setOnClickListener(v -> setWallpaper());
+    }
+
+    private void setWallpaper() {
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+
+        Glide.with(this)
+                .asBitmap()
+                .load(wallpaperUrl)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
+                        try {
+                            wallpaperManager.setBitmap(resource);
+                            Toast.makeText(FullscreenActivity.this, "Duvar kağıdı ayarlandı!", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            Toast.makeText(FullscreenActivity.this, "Hata: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                    }
+                });
     }
 }
